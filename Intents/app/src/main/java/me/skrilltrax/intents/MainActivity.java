ae8aa29b75da45c9d64ec43e264eafce9ba19c74
Aditya Wasan <adityawasan55@gmail.com>
2018-10-21 16:29:47 +0530

    RIP git log, Finally adding app

--- Intents/app/src/main/java/me/skrilltrax/intents/MainActivity.java
+++ Intents/app/src/main/java/me/skrilltrax/intents/MainActivity.java
@@ -1,13 +1,62 @@
 package me.skrilltrax.intents;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_USERNAME = "me.skrilltrax.intents.EXTRA_USERNAME";
+    public static final String EXTRA_PASSWORD = "me.skrilltrax.intents.EXTRA_PASSWORD";
+    public static final int REQUEST_CLICK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText usernameEdit = findViewById(R.id.usernameEdit);
+        final EditText passwordEdit = findViewById(R.id.passwordEdit);
+
+        final Intent i = new Intent(this,secondActivity.class);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(usernameEdit.getText().toString()) && TextUtils.isEmpty(passwordEdit.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),"no username and password",Toast.LENGTH_SHORT).show();
+                } else if(TextUtils.isEmpty(usernameEdit.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),"no username",Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(passwordEdit.getText().toString())) {
+                    Toast.makeText(getApplicationContext(),"no password",Toast.LENGTH_SHORT).show();
+                } else {
+                    Bundle userInfo = new Bundle();
+                    userInfo.putString(EXTRA_USERNAME,usernameEdit.getText().toString());
+                    userInfo.putString(EXTRA_PASSWORD,passwordEdit.getText().toString());
+
+                    i.putExtras(userInfo);
+                    startActivityForResult(i,REQUEST_CLICK);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CLICK) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getData().toString();
+                Toast.makeText(getApplicationContext(),reply,Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
--- Intents/app/src/main/java/me/skrilltrax/intents/secondActivity.java
+++ Intents/app/src/main/java/me/skrilltrax/intents/secondActivity.java
@@ -1,7 +1,16 @@
 package me.skrilltrax.intents;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import static me.skrilltrax.intents.MainActivity.EXTRA_USERNAME;
 
 public class secondActivity extends AppCompatActivity {
 
@@ -9,5 +18,49 @@ public class secondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle getInfo;
+
+        Intent i = getIntent();
+        getInfo = i.getExtras();
+
+        TextView userText = new TextView(this);
+        userText.setText(getInfo.getString(EXTRA_USERNAME));
+        userText.setTextColor(Color.BLACK);
+        userText.setId(View.generateViewId());
+
+        RelativeLayout secondLayout = findViewById(R.id.secondActivity);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        params.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        params.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        secondLayout.addView(userText,params);
+
+        Button backButton = new Button(this);
+        backButton.setText("BACK");
+        backButton.setId(View.generateViewId());
+
+        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonParams.addRule(RelativeLayout.BELOW, userText.getId());
+        buttonParams.setMargins(0,500,0,0);
+
+        secondLayout.addView(backButton,buttonParams);
+        backButton.setText("BACK");
+
+        final Intent returnIntent = new Intent();
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                returnIntent.setData(Uri.parse("YO RETURNED YO"));
+                setResult(RESULT_OK,returnIntent);
+                finish();
+            }
+        });
     }
 }
