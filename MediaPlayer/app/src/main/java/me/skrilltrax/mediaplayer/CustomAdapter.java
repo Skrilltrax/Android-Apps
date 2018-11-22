76f2bbe6adfa54a5a72ba881a4e5568cc613b7dc
Aditya Wasan <adityawasan55@gmail.com>
2018-11-22 11:46:43 +0530

    Use Picasso instead of custom AsyncTask loader.
    This is still a WIP commit since the thumbnail images are not displaying properly, might switch to
    getthumbnail() in next implementation.

--- MediaPlayer/app/src/main/java/me/skrilltrax/mediaplayer/CustomAdapter.java
+++ MediaPlayer/app/src/main/java/me/skrilltrax/mediaplayer/CustomAdapter.java
@@ -1,10 +1,9 @@
 package me.skrilltrax.mediaplayer;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.media.Image;
-import android.media.ThumbnailUtils;
-import android.os.Message;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -15,21 +14,34 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import com.squareup.picasso.Cache;
+import com.squareup.picasso.OkHttp3Downloader;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import okhttp3.OkHttpClient;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
 
     public static final String TAG = "mediainfo";
-
-    Context context;
+    public static Picasso picassoWithCache;
     private ArrayList<MediaData> mediaDataArrayList;
+    private File httpCacheDirectory;
+    private okhttp3.Cache cache;
+    Context context;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView headingText;
         TextView subtitleText;
         ImageView previewImage;
-        Bitmap previewImageBitmap;
+        String thumbnailPath;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -37,7 +49,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             headingText = itemView.findViewById(R.id.text_heading);
             subtitleText = itemView.findViewById(R.id.text_subtitle);
             previewImage = itemView.findViewById(R.id.imageView);
-
+            thumbnailPath = null;
         }
 
         public View getView() {
@@ -49,6 +61,15 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     public CustomAdapter(ArrayList<MediaData> data, Context context) {
         this.mediaDataArrayList = data;
         this.context = context;
+        httpCacheDirectory = new File(context.getCacheDir(), "picasso-cache");
+        cache = new okhttp3.Cache(httpCacheDirectory, 15* 1024 * 1024);
+        try {
+            Log.e(TAG,String.valueOf(httpCacheDirectory));
+            OkHttpClient.Builder okHttpClientBuilder = new OkHttpClient.Builder().cache(cache);
+            picassoWithCache = new Picasso.Builder(context.getApplicationContext()).downloader(new OkHttp3Downloader(okHttpClientBuilder.build())).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @NonNull
@@ -68,36 +89,31 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         TextView headingText = myViewHolder.headingText;
         TextView subtitleText = myViewHolder.subtitleText;
         ImageView previewImage = myViewHolder.previewImage;
-        Bitmap[] previewImageThumb = new Bitmap[1];
+        String thumbnailPath = myViewHolder.thumbnailPath;
 
         headingText.setText(mediaDataArrayList.get(position).getMediaTitle());
         subtitleText.setText(mediaDataArrayList.get(position).getMediaPath());
-        new ImageLoader(context,myViewHolder.getView(), mediaDataArrayList.get(position).getMediaID()).execute();
-
-//        final android.os.Handler handler = new android.os.Handler() {
-//            @Override
-//            public void handleMessage(Message msg) {
-//
-//                if (previewImageThumb[0] != null)
-//                    previewImage.setImageBitmap(previewImageThumb[0]);
-//            }
-//        };
-//
-//        Runnable r = new Runnable() {
-//            @Override
-//            public void run() {
-//                previewImageThumb[0] = ThumbnailUtils.createVideoThumbnail(subtitleText.getText().toString(),MediaStore.Video.Thumbnails.MICRO_KIND);
-//                Log.e(TAG,"CREATEEDDD");
-//                Log.d(TAG,subtitleText.toString());
-//                handler.sendEmptyMessage(0);
-//            }
-//
-//        };
+        thumbnailPath = mediaDataArrayList.get(position).getThumbnailPath();
+        picassoWithCache.load(Uri.fromFile(new File(thumbnailPath))).into(previewImage);
+        Log.e(TAG,String.valueOf(position));
+//        new ImageLoader(context,myViewHolder.getView(), mediaDataArrayList.get(position).getMediaID()).execute();
 
     }
 
     @Override
     public int getItemCount() {
-        return mediaDataArrayList.size();
+        return (mediaDataArrayList.size());
     }
+
+//    public Uri getUriFromBitmap(Context context, long currentId) {
+//
+//        ContentResolver contentResolver = context.getContentResolver();
+//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//        Bitmap previewImage;
+//        previewImage = MediaStore.Video.Thumbnails.getThumbnail(contentResolver, currentId, MediaStore.Video.Thumbnails.MINI_KIND,null);
+//        previewImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+//        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), previewImage, "Title", null);
+//
+//        return Uri.parse(path);
+//    }
 }
--- MediaPlayer/app/src/main/java/me/skrilltrax/mediaplayer/MediaData.java
+++ MediaPlayer/app/src/main/java/me/skrilltrax/mediaplayer/MediaData.java
@@ -4,11 +4,9 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.content.Context;
 import android.util.Log;
-import android.view.View;
 
 import java.util.ArrayList;
 
@@ -17,21 +15,29 @@ public class MediaData {
     public static final String TAG = "mediainfo";
 
     private long mediaID;
+    private long thumnbailID;
     private String mediaTitle;
     private String mediaPath;
-    private Bitmap mediaThumb;
+    private String thumbnailPath;
 
     String[] projection = {
             MediaStore.Video.Media._ID,
             MediaStore.Video.Media.TITLE,
-            MediaStore.Video.Media.DATA
+            MediaStore.Video.Media.DATA,
     };
 
-    public MediaData(long mediaID, String mediaTitle, String mediaPath) {
+    String[] thumbnailProjection = {
+            MediaStore.Video.Thumbnails._ID,
+            MediaStore.Video.Thumbnails.VIDEO_ID,
+            MediaStore.Video.Thumbnails.DATA
+    };
+
+
+    public MediaData(long mediaID, String mediaTitle, String mediaPath, String thumbnailPath) {
         this.mediaID = mediaID;
         this.mediaTitle = mediaTitle;
         this.mediaPath = mediaPath;
-//        this.mediaThumb = mediaThumb;
+        this.thumbnailPath = thumbnailPath;
     }
 
     public MediaData() {
@@ -50,7 +56,7 @@ public class MediaData {
         return mediaPath;
     }
 
-    public Bitmap getMediaThumb() { return mediaThumb; }
+    public String getThumbnailPath() { return thumbnailPath; }
 
     public ArrayList<MediaData> getMediaInfo(Context context) {
 
@@ -58,26 +64,33 @@ public class MediaData {
         ContentResolver contentResolver = context.getContentResolver();
         Uri mediaUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
         Cursor mediaCursor = contentResolver.query(mediaUri, projection, null, null, MediaStore.Video.Media.DEFAULT_SORT_ORDER);
+        Uri thumbnailUri = MediaStore.Video.Thumbnails.EXTERNAL_CONTENT_URI;
+        Cursor thumbnailCursor = contentResolver.query(thumbnailUri, thumbnailProjection, null, null, MediaStore.Video.Thumbnails.VIDEO_ID + " DESC" );
 
-        Bitmap previewImage;
 
-        if(mediaCursor != null && mediaCursor.moveToFirst())
+        if((mediaCursor != null && mediaCursor.moveToFirst()) && (thumbnailCursor != null && thumbnailCursor.moveToFirst()))
         {
             int mediaId = mediaCursor.getColumnIndex(MediaStore.Video.Media._ID);
             int mediaTitle = mediaCursor.getColumnIndex(MediaStore.Video.Media.TITLE);
             int mediaPath = mediaCursor.getColumnIndex(MediaStore.Video.Media.DATA);
 
+            int thumbnailId = thumbnailCursor.getColumnIndex(MediaStore.Video.Thumbnails._ID);
+            int thumbnailPath = thumbnailCursor.getColumnIndex(MediaStore.Video.Thumbnails.DATA);
             do {
+
                 long currentId = mediaCursor.getLong(mediaId);
+                long mediaID = thumbnailCursor.getLong(thumbnailId);
                 String currentTitle = mediaCursor.getString(mediaTitle);
                 String currentPath = mediaCursor.getString(mediaPath);
+                String currentThumbnailPath = thumbnailCursor.getString(thumbnailPath);
 //                previewImage = MediaStore.Video.Thumbnails.getThumbnail(contentResolver, currentId, MediaStore.Video.Thumbnails.MICRO_KIND, null);
 //                Log.e(TAG,"Got thumbnail");
 //                arrayList.add(new MediaData(currentId, currentTitle, currentPath, previewImage));
-                if (currentPath.contains("WhatsApp"));
-                arrayList.add(new MediaData(currentId, currentTitle, currentPath));
+                arrayList.add(new MediaData(currentId, currentTitle, currentPath, currentThumbnailPath));
                 Log.d(TAG,currentPath);
-            } while(mediaCursor.moveToNext());
+                mediaCursor.moveToNext();
+                thumbnailCursor.moveToNext();
+            } while(!mediaCursor.isAfterLast() && !thumbnailCursor.isAfterLast());
         }
         Log.d(TAG,arrayList.toString());
         return arrayList;
